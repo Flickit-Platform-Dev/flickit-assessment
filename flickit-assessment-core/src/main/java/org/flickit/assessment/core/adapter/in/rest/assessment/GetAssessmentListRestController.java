8cc40534b22ff079a27f8ca77ee61843567b64b9
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-21 13:47:08 +0330

    FAC-418:feature:Update GetAssessmentListRestController

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/GetAssessmentListRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/GetAssessmentListRestController.java
@@ -2,6 +2,7 @@ package org.flickit.assessment.core.adapter.in.rest.assessment;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
+import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.core.application.port.in.assessment.GetAssessmentListUseCase;
 import org.flickit.assessment.core.application.port.in.assessment.GetAssessmentListUseCase.AssessmentListItem;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @Validated
 @RestController
@@ -19,6 +21,7 @@ import java.util.List;
 public class GetAssessmentListRestController {
 
     private final GetAssessmentListUseCase useCase;
+    private final UserContext userContext;
 
     @GetMapping("/assessments")
     public ResponseEntity<PaginatedResponse<AssessmentListItem>> getAssessmentList(
@@ -27,11 +30,12 @@ public class GetAssessmentListRestController {
         @RequestParam(value = "kitId", required = false) Long kitId,
         @RequestParam(defaultValue = "10") int size,
         @RequestParam(defaultValue = "0") int page) {
-        PaginatedResponse<AssessmentListItem> result = useCase.getAssessmentList(toParam(spaceIds, kitId, size, page));
+        var currentUserId = userContext.getUser().id();
+        PaginatedResponse<AssessmentListItem> result = useCase.getAssessmentList(toParam(spaceIds, kitId, currentUserId, size, page));
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    private GetAssessmentListUseCase.Param toParam(List<Long> spaceIds, Long kitId, int size, int page) {
-        return new GetAssessmentListUseCase.Param(spaceIds, kitId, size, page);
+    private GetAssessmentListUseCase.Param toParam(List<Long> spaceIds, Long kitId, UUID currentUserId, int size, int page) {
+        return new GetAssessmentListUseCase.Param(spaceIds, kitId, currentUserId, size, page);
     }
 }
