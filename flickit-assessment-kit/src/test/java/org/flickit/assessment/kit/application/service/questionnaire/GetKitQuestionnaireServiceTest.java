bc2cbbebb907f99f7055d61a1dcdc7a4c2ff50f7
mahdirezazadeh <riza.rs2000@gmail.com>
2024-07-06 15:34:03 +0330

    FAC-508:feature:Rename to GetKitQuestionnaireServiceTest

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/questionnaire/GetKitQuestionnaireServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/questionnaire/GetKitQuestionnaireServiceTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class GetKitQuestionnaireUseCaseTest {
+class GetKitQuestionnaireServiceTest {
 
     @InjectMocks
     private GetKitQuestionnaireService service;
